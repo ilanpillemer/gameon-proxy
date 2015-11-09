c6d82dee4c967fdb95a40978c1b3ce43af1b793a
Ben Smith <benjsmi@us.ibm.com>
2015-11-09 16:08:18 -0500

    Made way for another process, but there was none.

--- startup.sh
+++ startup.sh
@@ -4,5 +4,5 @@ echo Replacing password in...
 sed -i s/PLACEHOLDER_PASSWORD/$ADMIN_PASSWORD/g /etc/haproxy/haproxy.cfg
 sed -i s/PLACEHOLDER_DOCKERHOST/$PROXY_DOCKER_HOST/g /etc/haproxy/haproxy.cfg
 sed -i s/PLACEHOLDER_LOGHOST/$LOGGING_DOCKER_HOST/g /etc/haproxy/haproxy.cfg
-echo Starting haproxy in the background...
+echo Starting haproxy-- foreground mode.
 haproxy -f /etc/haproxy/haproxy.cfg
