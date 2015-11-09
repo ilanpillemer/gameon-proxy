8a838f8d5699893927b7aab6b114dd8aef9d2a99
Ben Smith <benjsmi@us.ibm.com>
2015-11-09 09:50:22 -0500

    Should be able to just fix the build and be done with this one.

--- idsBuild.sh
+++ idsBuild.sh
@@ -6,12 +6,16 @@
 
 echo Informing slack...
 curl -X 'POST' --silent --data-binary '{"text":"A new build for the proxy has started."}' $WEBHOOK > /dev/null
-echo Downloading the Docker binary...
-wget http://game-on.org:8081/docker -O ./docker -q
+mkdir dockercfg ; cd dockercfg
+echo Downloading Docker requirements..
+wget http://docker-2.game-on.org:8081/dockerneeds.tar -q
+echo Setting up Docker...
+tar xzf dockerneeds.tar ; mv docker ../ ; cd .. ; chmod +x docker ; \
+	export DOCKER_HOST="tcp://docker-2.game-on.org:2375" DOCKER_TLS_VERIFY=1 DOCKER_CONFIG=./dockercfg
+
 echo Downloading the certificate...
 wget http://game-on.org:8081/proxy.pem -O ./proxy.pem -q
-chmod +x docker
-export DOCKER_HOST="tcp://game-on.org:2375"
+
 echo Building the docker image...
 ./docker build -t gameon-proxy .
 echo Stopping the existing container...
@@ -19,3 +23,5 @@ echo Stopping the existing container...
 ./docker rm gameon-proxy
 echo Starting the new container...
 ./docker run -d -p 80:80 -p 443:443 -p 1936:1936 -e ADMIN_PASSWORD=$ADMIN_PASSWORD -e PROXY_DOCKER_HOST=$PROXY_DOCKER_HOST --name=gameon-proxy gameon-proxy
+
+rm -rf docker-cfg
