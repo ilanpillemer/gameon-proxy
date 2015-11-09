31b4b32e2c667dbcef819a23f0f7f237d4d56d02
Ben Smith <benjsmi@us.ibm.com>
2015-11-09 16:00:50 -0500

    Proxy has been down this whole time.

--- startup.sh
+++ startup.sh
@@ -3,8 +3,6 @@
 echo Replacing password in...
 sed -i s/PLACEHOLDER_PASSWORD/$ADMIN_PASSWORD/g /etc/haproxy/haproxy.cfg
 sed -i s/PLACEHOLDER_DOCKERHOST/$PROXY_DOCKER_HOST/g /etc/haproxy/haproxy.cfg
-sed -i s/PLACEHOLDER_DOCKERHOST/$LOGGING_DOCKER_HOST/g /opt/filebeat-1.0.0-rc1-x86_64/filebeat.yml
+sed -i s/PLACEHOLDER_LOGHOST/$LOGGING_DOCKER_HOST/g /etc/haproxy/haproxy.cfg
 echo Starting haproxy in the background...
 haproxy -f /etc/haproxy/haproxy.cfg
-echo Starting Filebeat...
-/opt/filebeat-1.0.0-rc1-x86_64/filebeat -e -c /opt/filebeat-1.0.0-rc1-x86_64/file
