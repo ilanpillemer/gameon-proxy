b89873ec8e7901c8ab99900c0fd00247fd27d848
Ben Smith <benjsmi@us.ibm.com>
2015-11-20 12:57:28 -0500

    Those downloads needn't be so noisy.

--- idsBuild.sh
+++ idsBuild.sh
@@ -8,13 +8,13 @@ echo Informing slack...
 curl -X 'POST' --silent --data-binary '{"text":"A new build for the proxy has started."}' $WEBHOOK > /dev/null
 mkdir dockercfg ; cd dockercfg
 echo Downloading Docker requirements..
-wget --user=admin --password=$ADMIN_PASSWORD https://$BUILD_DOCKER_HOST:8443/dockerneeds.tar
+wget --user=admin --password=$ADMIN_PASSWORD https://$BUILD_DOCKER_HOST:8443/dockerneeds.tar -q
 echo Setting up Docker...
 tar xzf dockerneeds.tar ; mv docker ../ ; cd .. ; chmod +x docker ; \
 	export DOCKER_HOST="tcp://$BUILD_DOCKER_HOST:2376" DOCKER_TLS_VERIFY=1 DOCKER_CONFIG=./dockercfg
 
 echo Downloading the certificate...
-wget --user=admin --password=$ADMIN_PASSWORD https://$BUILD_DOCKER_HOST:8443/proxy.pem -O proxy.pem
+wget --user=admin --password=$ADMIN_PASSWORD https://$BUILD_DOCKER_HOST:8443/proxy.pem -O proxy.pem -q
 
 echo Building the docker image...
 ./docker build -t gameon-proxy .
